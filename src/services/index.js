10d4a24aba0e0321208f652a7a9b7bc511af77fb
Mathew <mathewharvey@gmail.com>
2020-12-03 21:33:00 +0800

    customised hooks and messages services

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable linebreak-style */
 const users = require('./users/users.service.js');
+const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(messages);
+// eslint-disable-next-line linebreak-style
 };
